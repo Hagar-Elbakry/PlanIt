92a561d88d6ca4a29d16890dc200909f3c66370b
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-06 18:34:58 +0200

    add styling

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PlanIt</title>
-    <link rel="stylesheet" href=""/>
+    <link rel="stylesheet" href="assets/css/main.min.css"/>
 </head>
 
 <body>
